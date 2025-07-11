c759528318da2f0dd6786ceddca9ada3079fffae
ekals1210 <ekals1210@naver.com>
2025-07-11 10:49:01 +0900

    [Feature] React Hooks 연습

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import Effect from "./Effect.tsx";
+// import App from "./App.tsx";
+import Memo from "./Effect.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Effect />
+    <Memo />
   </StrictMode>
 );
